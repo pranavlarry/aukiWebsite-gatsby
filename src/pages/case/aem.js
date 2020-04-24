955e3c0387fdf1116f99f8f9bfcf9358a0f39357
sachin.n <s.shetty@77agency.com>
2020-04-24 18:21:19 +0530

    case study expand page logic done

--- src/pages/case/aem.js
+++ src/pages/case/aem.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {graphql,useStaticQuery} from 'gatsby'
+import HomeLayout from '../../template/homepage'
+import CaseStudyExpand from "../../components/case-study/caseStudyExpand";
 
 const AemCase = props=> {
 
+    const data = useStaticQuery(graphql`
+    query{
+        allMarkdownRemark(filter: {frontmatter: {tag: {eq: "aem"}}}) {
+          edges {
+            node {
+              frontmatter {
+                shortDesc
+                tag
+                title
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `)
+    data = [...data.allMarkdownRemark.edges];
+    if(props.location.state) {
+      const selected = props.location.state.selected;
+      let loc;
+      data.forEach((val,index)=> {
+        if(selected === val.node.fields.slug) {
+          loc=index; 
+          return;
+        }
+      });
+      const select = data.splice(loc,1);
+      data = [select[0],...data];
+    }
+
     return(
+        <HomeLayout>
         <h1>hii</h1>
+        </HomeLayout>
     )
 }
 
--- src/pages/case/ecom.js
+++ src/pages/case/ecom.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import HomeLayout from '../../template/homepage'
+import {graphql,useStaticQuery} from 'gatsby'
+import CaseStudyExpand from "../../components/case-study/caseStudyExpand";
+
 
 const EcomCase = props=> {
-
+    let data = useStaticQuery(graphql`
+    query{
+        allMarkdownRemark(filter: {frontmatter: {tag: {eq: "ecom"}}}) {
+          edges {
+            node {
+              frontmatter {
+                shortDesc
+                tag
+                title
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `);
+    data = [...data.allMarkdownRemark.edges];
+    if(props.location.state) {
+      const selected = props.location.state.selected;
+      let loc;
+      data.forEach((val,index)=> {
+        if(selected === val.node.fields.slug) {
+          loc=index; 
+          return;
+        }
+      });
+      const select = data.splice(loc,1);
+      data = [select[0],...data];
+    }
     return(
-        <h1>hii</h1>
+      <HomeLayout>
+      <h1>hii</h1>
+      </HomeLayout>
     )
 }
 
